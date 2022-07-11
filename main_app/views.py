114c2588c22a51194854ba4b4ebdc44821c98375
Sean Watson <sean_watson30@me.com>
2022-07-11 12:09:00 -0600

    adds login requirements and restrictions

--- main_app/views.py
+++ main_app/views.py
@@ -23,12 +23,14 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
+@login_required
 def classes_index(request):
   classes = Class.objects.all()
   instructor = Instructor.objects.all()
   return render(request, 'classes/index.html', { 'classes': classes, 'instructor': instructor })
   # add more here later
 
+@login_required
 def classes_detail(request, class_id):
   classes = Class.objects.get(id=class_id)
   students = Student.objects.exclude(id__in=classes.students.all().values_list('id'))
@@ -37,10 +39,12 @@ def classes_detail(request, class_id):
     'class': classes, 'students': students, 'instructor': instructor
   })
 
+@login_required
 def students_index(request):
   students = Student.objects.all()
   return render(request, 'students/index.html', { 'students': students })
 
+@login_required
 def students_detail(request, student_id):
   student = Student.objects.get(id=student_id)
   classes = Class.objects.exclude(id__in=student.classes.all().values_list('id'))
@@ -48,11 +52,13 @@ def students_detail(request, student_id):
     'student': student, 'classes': classes 
   })
 
+@login_required
 def instructors_index(request):
   instructors = Instructor.objects.all()
   # 
   return render(request, 'instructors/index.html', { 'instructors': instructors })
 
+@login_required
 def instructors_detail(request, instructor_id):
   instructor = Instructor.objects.get(id=instructor_id)
   classes = Class.objects.exclude(id__in=instructor.classes.all().values_list('id'))
@@ -63,28 +69,34 @@ def instructors_detail(request, instructor_id):
 
 # ________ Many-to-Many Associations __________
 
+@login_required
 def assoc_student(request, class_id, student_id):
   Class.objects.get(id=class_id).students.add(student_id)
   return redirect('classes_detail', class_id=class_id)
 
+@login_required
 def assoc_student_delete(request, class_id, student_id):
   Class.objects.get(id=class_id).students.remove(student_id)
   return redirect('classes_detail', class_id=class_id)
 
+@login_required
 def assoc_class(request, student_id, class_id):
   Student.objects.get(id=student_id).classes.add(class_id)
   return redirect('students_detail', student_id=student_id)
 
+@login_required
 def assoc_class_delete(request, student_id, class_id):
   Student.objects.get(id=student_id).classes.remove(class_id)
   return redirect('students_detail', student_id=student_id)
 
 
 # Add Instructors Associatios Here
+@login_required
 def assoc_instructor(request, class_id, instructor_id):
   Class.objects.get(id=class_id).instructors.add(instructor_id)
   return redirect('classes_detail', class_id=class_id)
 
+@login_required
 def assoc_instructor_delete(request, class_id, instructor_id):
   Class.objects.get(id=class_id).instructors.remove(instructor_id)
   return redirect('classes_detail', class_id=class_id)
@@ -111,49 +123,49 @@ def signup(request):
 
 # ________ Class Declaration CRUD Functionality / Classes __________
 
-class ClassCreate(CreateView):
+class ClassCreate(LoginRequiredMixin, CreateView):
   model = Class
   fields = ['title', 'studio', 'day_of_week', 'start_time', 'end_time', 'instructors']
   success_url = '/classes/'
 
-class ClassUpdate(UpdateView):
+class ClassUpdate(LoginRequiredMixin, UpdateView):
   model = Class
   fields = ['title', 'studio', 'day_of_week', 'start_time', 'end_time', 'instructors']
   success_url = '/classes/'
 
-class ClassDelete(DeleteView):
+class ClassDelete(LoginRequiredMixin, DeleteView):
   model = Class
   success_url = '/classes/'
 
 # ________ Class Declaration CRUD Functionality / Students __________
 
-class StudentCreate(CreateView):
+class StudentCreate(LoginRequiredMixin, CreateView):
   model = Student
   fields = ['name', 'email', 'phone', 'age', 'address', 'city', 'state', 'zip']
   success_url = '/students/'
 
-class StudentUpdate(UpdateView):
+class StudentUpdate(LoginRequiredMixin, UpdateView):
   model = Student
   fields = ['name', 'email', 'phone', 'age', 'address', 'city', 'state', 'zip']
   success_url = '/students/'
 
-class StudentDelete(DeleteView):
+class StudentDelete(LoginRequiredMixin, DeleteView):
   model = Student
   success_url = '/students/'
 
 # ________ Class Declaration CRUD Functionality / Instructor __________
 
-class InstructorCreate(CreateView):
+class InstructorCreate(LoginRequiredMixin, CreateView):
   model = Instructor
   fields = ['name', 'email', 'phone']
   success_url = '/instructors/'
 
-class InstructorUpdate(UpdateView):
+class InstructorUpdate(LoginRequiredMixin, UpdateView):
   model = Instructor
   fields = ['name', 'email', 'phone']
   success_url = '/instructors/'
 
-class InstructorDelete(DeleteView):
+class InstructorDelete(LoginRequiredMixin, DeleteView):
   model = Instructor
   success_url = '/instructors/'
 
